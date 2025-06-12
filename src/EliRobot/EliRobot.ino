f9efa637486f56893783fe3f8af69e10f1981cf2
Daniele Alberti <dani.albe@tiscali.it>
2025-06-12 22:09:29 +0200

    rese esterne soglie in funzione decodifica pulsanti

--- src/EliRobot/EliRobot.ino
+++ src/EliRobot/EliRobot.ino
@@ -1,11 +1,11 @@
 #include <Servo.h>
 Servo myservo1;
 Servo myservo2;
-bool button0 = 0;
-bool button1 = 0;
-bool button2 = 0;
-bool button3 = 0;
-bool button4 = 0;
+bool btnU, btnR, btnD, btnL, btnC = false;
+bool btnUR, btnDR, btnDL, btnUL, btnDummy = false;
+
+int thresholdsA0[] = { 0, 300, 550, 700, 790, 900 };
+int thresholdsA1[] = { 0, 300, 550, 700, 790, 900 };
 
 void setup() {
   Serial.begin(115200);
@@ -17,36 +17,34 @@ void loop() {
   //myservo1.write(val);
   //myservo2.write(val);
 
+  Serial.print("value A0: ");
+  Serial.println(analogRead(A0));
 
+  Serial.print("value A1 : ");
+  Serial.println(analogRead(A1));
 
-
-  decodeButton(analogRead(A0), &button0, &button1, &button2, &button3, &button4);
-
-  //Serial.print("A0: ");
-  //Serial.println(analogRead(A0));
-  //Serial.print("A1: ");
-  //Serial.println(analogRead(A1));
-  
-  Serial.println(button0);
+  decodeButton(analogRead(A0), thresholdsA0, &btnU, &btnR, &btnD, &btnL, &btnC);
+  decodeButton(analogRead(A1), thresholdsA1, &btnUR, &btnDR, &btnDL, &btnUL, &btnDummy);
 }
 
-void decodeButton(int value, bool *level0, bool *level1, bool *level2, bool *level3, bool *level4) {
 
-  *level0 = 0;
-  *level1 = 0;
-  *level2 = 0;
-  *level3 = 0;
-  *level4 = 0;
+// Funzione che decodifica il valore in base alle soglie
+void decodeButton(int value, const int thresholds[], bool *level0, bool *level1, bool *level2, bool *level3, bool *level4) {
+  *level0 = false;
+  *level1 = false;
+  *level2 = false;
+  *level3 = false;
+  *level4 = false;
 
-  if ((value >= 0) && (value <= 300)) {
+  if (value >= thresholds[0] && value < thresholds[1]) {
     *level0 = true;
-  } else if ((value >= 300) && (value <= 550)) {
+  } else if (value >= thresholds[1] && value < thresholds[2]) {
     *level1 = true;
-  } else if ((value >= 550) && (value <= 700)) {
+  } else if (value >= thresholds[2] && value < thresholds[3]) {
     *level2 = true;
-  } else if ((value >= 700) && (value <= 790)) {
+  } else if (value >= thresholds[3] && value < thresholds[4]) {
     *level3 = true;
-  } else if ((value >= 790) && (value <= 900)) {
+  } else if (value >= thresholds[4] && value <= thresholds[5]) {
     *level4 = true;
   }
 }
