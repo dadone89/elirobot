2a854e4502ea7edd85b9e8de0c2092030bbe1ade
Daniele Alberti <dani.albe@tiscali.it>
2025-06-15 11:55:29 +0200

    corretti tasti, primi test movimentazione

--- src/EliRobot/EliRobot.ino
+++ src/EliRobot/EliRobot.ino
@@ -5,7 +5,7 @@ bool btnU, btnR, btnD, btnL, btnC = false;
 bool btnUR, btnDR, btnDL, btnUL, btnDummy = false;
 
 int thresholdsA0[] = { 0, 300, 550, 700, 790, 900 };
-int thresholdsA1[] = { 0, 300, 550, 700, 790, 900 };
+int thresholdsA1[] = { 0, 400, 600, 720, 900, 1000 };
 
 void setup() {
   Serial.begin(115200);
@@ -17,14 +17,51 @@ void loop() {
   //myservo1.write(val);
   //myservo2.write(val);
 
-  Serial.print("value A0: ");
-  Serial.println(analogRead(A0));
+  //Serial.print("value A0: ");
+  //Serial.println(analogRead(A0));
 
-  Serial.print("value A1 : ");
-  Serial.println(analogRead(A1));
+  //Serial.print("value A1 : ");
+  //Serial.println(analogRead(A1));
 
-  decodeButton(analogRead(A0), thresholdsA0, &btnU, &btnR, &btnD, &btnL, &btnC);
-  decodeButton(analogRead(A1), thresholdsA1, &btnUR, &btnDR, &btnDL, &btnUL, &btnDummy);
+  decodeButton(analogRead(A0), thresholdsA0, &btnC, &btnDL, &btnUL, &btnUR, &btnDR);
+  decodeButton(analogRead(A1), thresholdsA1, &btnL, &btnU, &btnR, &btnD, &btnDummy);
+  
+  // Debug messages per A0 (primo joystick/gruppo di tasti)
+  if (btnU) {
+    Serial.println("DEBUG A0: Tasto UP premuto");
+  }
+  if (btnR) {
+    Serial.println("DEBUG A0: Tasto RIGHT premuto");
+  }
+  if (btnD) {
+    Serial.println("DEBUG A0: Tasto DOWN premuto");
+  }
+  if (btnL) {
+    Serial.println("DEBUG A0: Tasto LEFT premuto");
+  }
+  if (btnC) {
+    Serial.println("DEBUG A0: Tasto CENTER premuto");
+  }
+  
+  // Debug messages per A1 (secondo joystick/gruppo di tasti)
+  if (btnUR) {
+    Serial.println("DEBUG A1: Tasto UP-RIGHT premuto");
+  }
+  if (btnDR) {
+    Serial.println("DEBUG A1: Tasto DOWN-RIGHT premuto");
+  }
+  if (btnDL) {
+    Serial.println("DEBUG A1: Tasto DOWN-LEFT premuto");
+  }
+  if (btnUL) {
+    Serial.println("DEBUG A1: Tasto UP-LEFT premuto");
+  }
+  if (btnDummy) {
+    Serial.println("DEBUG A1: Tasto DUMMY premuto");
+  }
+  
+  delay(100); // Piccola pausa per evitare spam nel monitor seriale
+
 }
 
 
